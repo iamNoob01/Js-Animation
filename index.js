3bafca4319f61829e6da548487dbd124b6302108
IamNoob01 <96548302+iamNoob01@users.noreply.github.com>
2023-08-05 00:40:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var typed = new Typed('#heding', {
+     strings : ["JavaScript Animation","Animation JavaScript Reverse"],
+     typeSpeed : 60,
+     backSpeed :10,
+     loop : true
+});
+
+AOS.init({
+     duration : 2000
+});
